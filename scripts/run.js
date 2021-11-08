7129f0c60a69231ea31b7544b387ff0aa181cee4
Darshan H M <naikdarsh175@gmail.com>
2021-11-08 16:34:55 +0530

    added default characters through constructors

---
+++ scripts/run.js
@@ -0,0 +1,27 @@
+const main = async () => {
+	const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+	const gameContract = await gameContractFactory.deploy(
+		['Assassin', 'Swordsman', 'Sniper'], // Names
+		[
+			'https://i.imgur.com/H1Lno7t.png', // Images
+			'https://i.imgur.com/Tjcw9j6.png',
+			'https://i.imgur.com/louNZ3v.png',
+		],
+		[50, 150, 100], // HP values
+		[150, 50, 100] // Attack damage values
+	);
+	await gameContract.deployed();
+	console.log('Contract deployed to:', gameContract.address);
+};
+
+const runMain = async () => {
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
+};
+
+runMain();
