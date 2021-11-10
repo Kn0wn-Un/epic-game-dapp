2da90c5231b904e8f5739667323f7499274f642e
Darshan H M <naikdarsh175@gmail.com>
2021-11-10 22:26:41 +0530

    frontend added

---
+++ client/src/constants.js
@@ -0,0 +1,17 @@
+const CONTRACT_ADDRESS = '0xe607154C413Fd7DEeeC00F8de8e05050a8B765eA';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
+
